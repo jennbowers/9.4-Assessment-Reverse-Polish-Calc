cfa4eb7e0216a8e8cd46f679b81916ef0904c85d
Jenn Bowers <jenniferbowers03@gmail.com>
2017-08-03 23:12:38 -0400

    Committing rest of files that did not originally get pushed to github

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+/**
+ * Test Driven Development (TDD)
+ * Write your tests first and watch them fail.
+ * Then write the algorithm and watch the tests pass.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ReversePolishCalc rpc = new ReversePolishCalc();
+
+//        String rpnExpression = "2.5,4.8,+";
+//        double expectedResult = 7.3;
+//        double actualResult = rpc.calculate(rpnExpression);
+//        checkResult(rpnExpression, expectedResult, actualResult);
+
+
+        // Uncomment this test which has many operators
+        String rpnExpression = "5,1,2,+,4,*,+,3,-";
+        double expectedResult = 14.0;
+        double actualResult = rpc.calculate(rpnExpression);
+        checkResult(rpnExpression, expectedResult, actualResult);
+    }
+
+    private static void checkResult(String expression, double expected, double actual) {
+        if (expected == actual) {
+            System.out.println("SUCCESS: " + expression + " is " + expected);
+        } else {
+            System.out.println("ERROR: " + expression + " expected " + expected + " actual " + actual);
+        }
+    }
+}
